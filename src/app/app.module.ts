352a8cffc48cb349e6d0abee1e3900a57527cf96
azizhish <azizhish@gmail.com>
2018-07-11 17:47:10 +0400

    e2e testing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { HttpClientModule } from '@angular/common/http'
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { AppComponent } from './app.component'
-import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { CurrentWeatherComponent } from "./current-weather/current-weather.component";
 // Services imports
-import { WeatherService } from './weather/weather.service'
+import { WeatherService } from "./weather/weather.service";
+import { WeatherServiceFake } from "./weather/weather.service.fake";
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
   imports: [BrowserModule, HttpClientModule],
-  providers: [WeatherService],
-  bootstrap: [AppComponent],
+  providers: [WeatherService, WeatherServiceFake],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,7 +42,7 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect'
+import "core-js/es7/reflect";
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
@@ -64,7 +64,7 @@ import 'core-js/es7/reflect'
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone' // Included with Angular CLI.
+import "zone.js/dist/zone"; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
