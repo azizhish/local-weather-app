da2885bcb7c2c5e3c8e6e1c842fd273c4b8b9857
azizhish <azizhish@gmail.com>
2018-07-11 15:41:28 +0400

    Basic testing done

--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -1,10 +1,13 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { inject, TestBed } from '@angular/core/testing'
 import { WeatherService } from './weather.service'
+import { WeatherServiceFake } from './weather.service.fake'
 
 describe('WeatherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [WeatherService],
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
     })
   })
 
